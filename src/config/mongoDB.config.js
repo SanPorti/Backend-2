8074a66434352c4259710e9367f7a9f8b3d4281c
Santiago Portillo <snporti@gmail.com>
2024-11-20 15:17:26 -0300

    primer entrega

--- src/config/mongoDB.config.js
+++ src/config/mongoDB.config.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 
+const uriMongo = process.env.URI_MONGO;
+
 //Conexion a BD
 export const connectMongoDB = async () => {
   try {
-      mongoose.connect("mongodb+srv://admin:12345@cluster0.etdwv.mongodb.net/")
+      mongoose.connect(uriMongo)
       console.log("MongoDB conectado");
   } catch (error) {
     console.log(`Error: ${error}`);
